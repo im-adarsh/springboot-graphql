32fd725d4f4027895b95fcddc12e4e0b0f813048
Adarsh Kumar <adarshkumar9820@gmail.com>
2019-09-21 10:07:46 +0800

    init

---
+++ graphql-gateway-provider/src/main/java/com/clydoskope/graphql/provider/graphqlgatewayprovider/GraphqlGatewayProviderApplication.java
@@ -0,0 +1,13 @@
+package com.clydoskope.graphql.provider.graphqlgatewayprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlGatewayProviderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GraphqlGatewayProviderApplication.class, args);
+  }
+
+}
